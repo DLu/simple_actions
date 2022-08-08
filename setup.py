e0add07d3fc6eb8f4fa0bb0b24384bccf5cfee5d
David V. Lu <davidvlu@gmail.com>
2022-08-08 14:54:50 -0400

    Simple Actions Package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+package_name = 'simple_actions'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name, 'simple_actions'],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='David V. Lu!!',
+    maintainer_email='davidvlu@gmail.com',
+    description='Library for easier use of rclpy actions',
+    license='BSD',
+)
